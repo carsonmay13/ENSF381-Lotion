f7dbdaef5b6028b0b8bac6214003b71f3a841dfe
carson-may <carson.may@ucalgary.ca>
2023-02-28 19:31:18 -0700

    some minor changes

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import Note from './Note'
+import React from "react";
+import Note from "./Note";
 
 export default function NotesList({ notes }) {
   return (
-    <ol className = "notesList">
+    <ol className="notesList">
       {notes.map((note) => (
-        <Note id={note.id} title={note.title} text={note.text} date={note.date} key={note.id}/>
+        <Note
+          id={note.id}
+          title={note.title}
+          text={note.text}
+          date={note.date}
+          key={note.id}
+        />
       ))}
     </ol>
-  )
+  );
 }
